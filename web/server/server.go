24f19203715cef7bd7d356b04d5104fee65845df
Vítor Carrara <softwarecarrara@gmail.com>
2024-06-26 09:47:00 -0300

    navbar and styles

--- web/server/server.go
+++ web/server/server.go
@@ -1,53 +1,24 @@
- package server 
+package server
 
- import ( 
-     "html/template" 
-     "log" 
-     "net/http"
- )
+import (
+	"html/template"
+	"net/http"
+)
 
-type Project struct { 
-     name string 
-} 
-
-const oneProject = Project{"Project"}
-
-const MyProjects = []Project{ 
-    Project(name="Carrara Software"),
-    {name="LandingPageV1"}, 
-    {name="EhPrimo"}
-}
 func Serve() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/projects", projects)
+	http.HandleFunc("/", handler)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("web/template/assets/"))))
 	http.ListenAndServe("localhost:8000", nil)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles(
-		"web/template/base.tmpl",
-		"web/template/header.tmpl",
-		"web/template/home.tmpl",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tmpl.Execute(w, nil)
-}
-
-func projects(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles(
+func handler(w http.ResponseWriter, r *http.Request) {
+	base := template.Must(template.ParseFiles(
 		"web/template/base.html",
 		"web/template/header.html",
-		"web/template/projects.html",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+		"web/template/nav.html",
+	))
+    path := r.URL.Path
+    context := make(map[string]string)
+    context["Location"] = path
+	base.Execute(w, context)
 }
-
-
-
-
-
